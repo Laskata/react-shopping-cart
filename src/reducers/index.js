ca065e27fde6d5f1d0f6737c8bfd931236e8a9a8
Laze <lazeto8@gmail.com>
2021-07-05 17:08:44 +0200

    add redux to cart

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import { cartReducer } from "./cartReducers";
 import { productsReducer } from "./productReducers";
 
 export default combineReducers({
   products: productsReducer,
+  cart: cartReducer,
 });
