cbb20d30a796ec1d0be3c6ff3ee137f4f3338379
Laze <lazeto8@gmail.com>
2021-07-09 12:24:04 +0200

    added routing and login form

--- src/App.js
+++ src/App.js
@@ -2,26 +2,45 @@ import React, { useState } from "react";
 import Products from "./components/Products";
 import Filter from "./components/Filter";
 import Cart from "./components/Cart";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import Login from "./components/screens/Login";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React Shopping Cart</a>
-        <a href="/">Sign In</a>
-      </header>
-      <main>
-        <div className="content">
-          <div className="main">
-            <Filter />
-            <Products />
+    <BrowserRouter>
+      <div className="grid-container">
+        <header>
+          <div>
+            <Link to="/">React Shopping Cart</Link>
           </div>
-          <div className="sidebar">Cart Items</div>
-          <Cart />
-        </div>
-      </main>
-      <footer>All rights is reserved.</footer>
-    </div>
+          <div className="second-header-links">
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/login">Sign In</Link>
+          </div>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Route path="/" exact component={Filter} />
+              <Route path="/" exact component={Products} />
+              <Route path="/login" component={Login} />
+              <Route path="/cart" component={Cart} />
+            </div>
+            {/* <div className="sidebar">Cart Items</div> */}
+          </div>
+        </main>
+        <footer>All rights is reserved.</footer>
+      </div>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/screens/Login.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form className="login-form" onSubmit={submitHandler}>
+        <div>
+          <h1>Log In</h1>
+        </div>
+        <div>
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <label />
+          <button className="button primary" type="submit">
+            Log In
+          </button>
+        </div>
+        <div>
+          <label />
+          <div>
+            New customer? <Link to="/register">Create your account</Link>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
